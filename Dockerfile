0d2ce900a62937907bf67283ac11666ea3cb3e64
Thijs Haarhuis <thijs.haarhuis@qenvent.com>
2021-05-05 11:47:43 +0200

    Make sure it actually works.

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,12 @@ COPY requirements.txt influx.py /app/
 RUN pip install -r /app/requirements.txt \
  && chmod 755 influx.py
 
- ENV LYRIC_CLIENT_ID=
+ENV LYRIC_CLIENT_ID=
 ENV LYRIC_CLIENT_SECRET=
 ENV LYRIC_APP_NAME=
 ENV LYRIC_REDIRECT_UI=
 ENV LYRIC_TOKEN=
-ENV INFLUX_HOST=
+ENV INFLUX_URL=
 ENV INFLUX_DB=
 
 CMD ["python3", "-u", "/app/influx.py"]
--- influx.py
+++ influx.py
@@ -3,30 +3,29 @@
 from dotenv import load_dotenv
 load_dotenv()
 import os
+import json
 
-""" Honeywell API Settings """
+""" Lyric API Settings """
 LYRIC_CLIENT_ID = os.environ['LYRIC_CLIENT_ID']
 LYRIC_CLIENT_SECRET = os.environ['LYRIC_CLIENT_SECRET']
 LYRIC_APP_NAME = os.environ['LYRIC_APP_NAME']
 LYRIC_REDIRECT_UI = os.environ['LYRIC_REDIRECT_UI']
-LYRIC_TOKEN = os.environ['LYRIC_TOKEN']
+LYRIC_TOKEN = json.loads(os.environ['LYRIC_TOKEN'])
 
 """ Influx Settings """
-INFLUX_HOST     = os.environ['INFLUX_HOST']
-INFLUX_DB       = os.environ['INFLUX_DB']
-""" Optional Influx Settings """
-INFLUX_PORT     = os.environ.get('INFLUX_PORT', 8086)
-INFLUX_USERNAME = os.environ.get('INFLUX_USERNAME',None)
-INFLUX_PASSWORD = os.environ.get('INFLUX_PASSWORD',None)
+INFLUX_URL = os.environ['INFLUX_URL']
+INFLUX_DB = os.environ['INFLUX_DB']
+INFLUX_USERNAME = os.environ.get('INFLUX_USERNAME','')
+INFLUX_PASSWORD = os.environ.get('INFLUX_PASSWORD','')
 
-POLL_INTERVAL = os.environ.get('POLL_INTERVAL', 120)
-
-DEBUG_MODE = os.environ.get('DEBUG_MODE', False)
+POLL_INTERVAL = int(os.environ.get('POLL_INTERVAL', 120))
+DEBUG_MODE = bool(os.environ.get('DEBUG_MODE', False))
 
 from time import sleep
 from lyric import Lyric
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient,Point
 import logging
+
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 logger = logging.getLogger()
 if DEBUG_MODE:
@@ -34,31 +33,35 @@ if DEBUG_MODE:
 else:
     logger.setLevel(logging.INFO)
 
-
 def loop():
     lapi = Lyric(
-      client_id = client_id,
-      client_secret = client_secret,
-      token_cache_file = token_cache_file,
-      redirect_uri = redirect_uri,
-      app_name = app_name)
+      client_id = LYRIC_CLIENT_ID,
+      client_secret = LYRIC_CLIENT_SECRET,
+      token = LYRIC_TOKEN,
+      redirect_uri = LYRIC_REDIRECT_UI,
+      app_name = LYRIC_APP_NAME)
 
-    influxClient = InfluxDBClient(host=INFLUX_HOST, port=INFLUX_PORT,username=INFLUX_USERNAME,password=INFLUX_PASSWORD)
-    influxClient.switch_database(INFLUX_DB)
+    token = ""
+    if INFLUX_USERNAME and INFLUX_PASSWORD:
+        token = "{}:{}".format(INFLUX_USERNAME,INFLUX_PASSWORD)
+
+    logger.info(f"Influx url:{INFLUX_URL}")
     
-    while(True):
-      for location in lapi.locations:  
-        for thermostat in location.thermostats:
-            uniqueId = "{}:{}".format(location.name,thermostat.name)
-            indoorTemperature = float(thermostat.indoorTemperature)
-            heatSetPoint = float(thermostat.heatSetpoint)  
-            influxBody = [{"measurement":uniqueId,"fields":{"indoorTemperature":indoorTemperature,"heatSetpoint":heatSetPoint}}]
-            influxClient.write_points(influxBody)
-      sleep(POLL_INTERVAL)
-    
+    bucket = INFLUX_DB
+    with InfluxDBClient(url=INFLUX_URL,token=token,org='-') as client:
+        with client.write_api() as write_api:
+            while(True):
+              for location in lapi.locations:  
+                for thermostat in location.thermostats:
+                    uniqueId = "{}:{}".format(location.name,thermostat.name)
+                    indoorTemperature = float(thermostat.indoorTemperature)
+                    temperatureSetPoint = float(thermostat.temperatureSetpoint)  
+                    point = Point(uniqueId).field("indoorTemperature",indoorTemperature).field("temperatureSetPoint",temperatureSetPoint)                    
+                    write_api.write(bucket=bucket,record=point)
+              sleep(POLL_INTERVAL)
 
 if __name__ == '__main__':
-    logger.info("Start")
+    logger.info("Starting")
     try:
         loop()
     except KeyboardInterrupt:        
